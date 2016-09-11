1af53c419bd3ef3517e9c071e8bfc7ea8e65ff6a
Afanasyev Ivan <ivafanas@gmail.com>
2016-09-11 18:33:18 +0600

    [ REF ] simplify function_with_fixture_and_args_generator example

--- examples/function_with_fixture_and_args_generator.cpp
+++ examples/function_with_fixture_and_args_generator.cpp
@@ -5,69 +5,68 @@
 #include <vector>
 
 
-class NSortFunctionArgsGenerator
+namespace {
+
+class Generator
 {
 public:
-	struct SortFunctionArg
-	{
-		size_t count;
-		size_t ncalls;
-	};
+    struct ArgType
+    {
+        size_t size;
+        size_t ncalls;
+    };
 
-	typedef SortFunctionArg ArgType;
+    Generator() {}
 
-	NSortFunctionArgsGenerator() {}
-
-	std::vector<ArgType> Generate(int argc, char **argv)
-	{
-		// You may get input filename from argc,argv and
-		// read your custom file and fill arguments vector.
-		//
-		// Or arguments might be passed via command line arguments, it is up to You.
-		//
-		// However, if the input dataset is known at compile-time,
-		// you would better use SLTBENCH_FUNCTION_WITH_FIXTURE_AND_ARGS macro
-		//
-		// I will just left hardcoded solution here
-		return{ { 100000, 3 },{ 110000, 4 },{ 120000, 5 } };
-	}
+    std::vector<ArgType> Generate(int argc, char **argv)
+    {
+        // You may get input filename from argc,argv and
+        // read your custom file and fill arguments vector.
+        //
+        // Or arguments might be passed via command line arguments, it is up to You.
+        //
+        // However, if the input dataset is known at compile-time,
+        // you would better use SLTBENCH_FUNCTION_WITH_FIXTURE_AND_ARGS macro
+        //
+        // I will just left hardcoded solution here
+        return{{100000, 10}, {200000, 20}, {300000, 30}};
+    }
 };
 
-std::ostream& operator << (std::ostream& os, const NSortFunctionArgsGenerator::SortFunctionArg& rhs)
+std::ostream& operator << (std::ostream& os, const Generator::ArgType& rhs)
 {
-	return os << rhs.count << '/' << rhs.ncalls;
+    return os << rhs.ncalls << '/' << rhs.size;
 }
 
-class NSortFunctionFixture
+class Fixture
 {
 public:
-	typedef std::vector<size_t> Type;
+    typedef std::vector<size_t> Type;
 
-	NSortFunctionFixture() {}	
-	
-	Type& SetUp(const NSortFunctionArgsGenerator::ArgType& arg)
-	{
-		const size_t size = arg.count;
-		fixture_.resize(size, 0);
-		for (size_t i = 0; i < size; ++i)
-		{
-			fixture_[i] = size - i;
-		}
-		return fixture_;
-	}
+    Fixture() {}
 
-	void TearDown() {}
+    Type& SetUp(const Generator::ArgType& arg)
+    {
+        fixture_.resize(arg.size, 0);
+        for (size_t i = 0; i < arg.size; ++i)
+            fixture_[i] = i;
+        return fixture_;
+    }
+
+    void TearDown() {}
 
 private:
-	Type fixture_;
+    Type fixture_;
 };
 
-void NSortFunctionWGenerator(NSortFunctionFixture::Type& fixture, const NSortFunctionArgsGenerator::ArgType& arg)
+void ShuffleWithGenerator(Fixture::Type& fix, const Generator::ArgType& arg)
 {
-	// some useful work here based on fixture and arg
-	for (size_t i = 0; i < arg.ncalls; ++i)
-	{
-		std::sort(fixture.begin(), fixture.end());
-	}
+    // some useful work here based on fixture and arg
+    for (size_t i = 0; i < arg.ncalls; ++i)
+    {
+        std::random_shuffle(fix.begin(), fix.end());
+    }
 }
-SLTBENCH_FUNCTION_WITH_FIXTURE_AND_ARGS_GENERATOR(NSortFunctionWGenerator, NSortFunctionFixture, NSortFunctionArgsGenerator);
+SLTBENCH_FUNCTION_WITH_FIXTURE_AND_ARGS_GENERATOR(ShuffleWithGenerator, Fixture, Generator);
+
+} // namespace
