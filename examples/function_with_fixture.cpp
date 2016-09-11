e2abdbab688f08c8c9fbee18c6c4f802eeec0dbc
Afanasyev Ivan <ivafanas@gmail.com>
2016-09-11 18:31:18 +0600

    [ REF ] simplify function_with_fixture example

--- examples/function_with_fixture.cpp
+++ examples/function_with_fixture.cpp
@@ -4,33 +4,37 @@
 #include <vector>
 
 
-class SortFunctionFixture
+namespace {
+
+class Fixture
 {
 public:
-	typedef std::vector<size_t> Type;
+    typedef std::vector<size_t> Type;
 
-	SortFunctionFixture() {}	
-	
-	Type& SetUp()
-	{
-		const size_t size = 100000;
-		fixture_.resize(size, 0);
-		for (size_t i = 0; i < size; ++i)
-		{
-			fixture_[i] = size - i;
-		}
-		return fixture_;
-	}
+    Fixture() {}
 
-	void TearDown() {}
+    Type& SetUp()
+    {
+        const size_t size = 100000;
+        fixture_.resize(size, 0);
+        for (size_t i = 0; i < size; ++i)
+        {
+            fixture_[i] = size - i;
+        }
+        return fixture_;
+    }
+
+    void TearDown() {}
 
 private:
-	Type fixture_;
+    Type fixture_;
 };
 
 
-void SortFunction(SortFunctionFixture::Type& fixture)
+void Sort(Fixture::Type& fix)
 {
-	std::sort(fixture.begin(), fixture.end());
+    std::sort(fix.begin(), fix.end());
 }
-SLTBENCH_FUNCTION_WITH_FIXTURE(SortFunction, SortFunctionFixture);
+SLTBENCH_FUNCTION_WITH_FIXTURE(Sort, Fixture);
+
+} // namespace
