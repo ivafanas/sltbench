7bbe350675a9379811664c98b44f417708ac11ce
Afanasyev Ivan <ivafanas@gmail.com>
2016-09-11 18:29:49 +0600

    [ REF ] simplify function_with_args_generator example

--- examples/function_with_args_generator.cpp
+++ examples/function_with_args_generator.cpp
@@ -5,50 +5,45 @@
 #include <vector>
 
 
-class SortFunctionArgsGenerator
+namespace {
+
+class Generator
 {
 public:
-	struct SortFunctionArg
-	{
-		size_t count;
-		size_t n_sorts;
-	};
+    struct ArgType
+    {
+        std::string src;
+        size_t n;
+    };
 
-	typedef SortFunctionArg ArgType;
+    Generator() {}
 
-	SortFunctionArgsGenerator() {}
-
-	std::vector<ArgType> Generate(int argc, char **argv)
-	{
-		// You may get input filename from argc,argv and
-		// read your custom file and fill arguments vector.
-		//
-		// Or arguments might be passed via command line arguments, it is up to You.
-		//
-		// However, if the input dataset is known at compile-time,
-		// you would better use SLTBENCH_FUNCTION_WITH_ARGS macro
-		//
-		// I will just left hardcoded solution here
-		return { { 100000, 3 }, { 110000, 4 }, { 120000, 5 } };
-	}
+    std::vector<ArgType> Generate(int argc, char **argv)
+    {
+        // You may get input filename from argc,argv and
+        // read your custom file and fill arguments vector.
+        //
+        // Or arguments might be passed via command line arguments, it is up to You.
+        //
+        // However, if the input dataset is known at compile-time,
+        // you would better use SLTBENCH_FUNCTION_WITH_ARGS macro
+        //
+        // I will just left hardcoded solution here
+        return{{"knowledge", 100000}, {"power", 200000}};
+    }
 };
 
-std::ostream& operator << (std::ostream& os, const SortFunctionArgsGenerator::SortFunctionArg& rhs)
+std::ostream& operator << (std::ostream& os, const Generator::ArgType& rhs)
 {
-	return os << rhs.count << '/' << rhs.n_sorts;
+    return os << rhs.n << '/' << rhs.src;
 }
 
-void SortFunctionWithArgGenerator(const SortFunctionArgsGenerator::ArgType& arg)
+void StringMultWithGenerator(const Generator::ArgType& arg)
 {
-	// let's propose we are going to do something useful computations here
-	std::vector<size_t> vec(arg.count, 0);
-	for (size_t i = 0; i < vec.size(); ++i)
-	{
-		vec[i] = vec.size() - i;
-	}
-	for (size_t i = 0; i < arg.n_sorts; ++i)
-	{
-		std::sort(vec.begin(), vec.end());
-	}
+    std::string rv;
+    for (size_t i = 0; i < arg.n; ++i)
+        rv += arg.src;
 }
-SLTBENCH_FUNCTION_WITH_ARGS_GENERATOR(SortFunctionWithArgGenerator, SortFunctionArgsGenerator);
+SLTBENCH_FUNCTION_WITH_ARGS_GENERATOR(StringMultWithGenerator, Generator);
+
+} // namespace
