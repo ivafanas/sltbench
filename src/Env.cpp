72002a65278c4ec607d1f22ea8d010477405fc57
Ivan Afanasyev <ivafanas@gmail.com>
2017-01-05 19:43:32 +0700

    [ ADD ] add --reporter command line option

--- src/Env.cpp
+++ src/Env.cpp
@@ -1,19 +1,30 @@
 #include <sltbench/impl/Env.h>
 #include <sltbench/impl/IConfig.h>
 
+#include "ConsoleReporter.h"
+#include "CsvReporter.h"
 #include "Filters.h"
+#include "JsonReporter.h"
 #include "ProgramOptions.h"
 
+#include <iostream>
 #include <memory>
 
 
 namespace sltbench {
 namespace {
 
+template<typename T>
+std::unique_ptr<T> make_uniq()
+{
+	return std::unique_ptr<T>(new T);
+}
+
 void InitConfig(const int argc, char **argv)
 {
 	auto options_to_values = BuildProgramOptions(argc, argv);
 
+	// setup test cases filter
 	const auto filter_expr = options_to_values["--filter"];
 	if (!filter_expr.empty())
 	{
@@ -24,9 +35,31 @@ void InitConfig(const int argc, char **argv)
 		GetConfig().GetPrivate().SetFilter(std::move(filter));
 	}
 
+	// setup if heatup required
 	const auto heatup_value = options_to_values["--heatup"];
-	if (heatup_value == "off" || heatup_value == "OFF")
-		GetConfig().GetPrivate().SetHeatupRequired(false);
+	if (!heatup_value.empty())
+	{
+		if (heatup_value == "off" || heatup_value == "OFF")
+			GetConfig().GetPrivate().SetHeatupRequired(false);
+		else if (heatup_value == "on" || heatup_value == "ON")
+			GetConfig().GetPrivate().SetHeatupRequired(true);
+		else
+			std::cerr << "ERROR: Unknown heatup option value: " << heatup_value << '\n';
+	}
+
+	// setup output format
+	const auto reporter_value = options_to_values["--reporter"];
+	if (!reporter_value.empty())
+	{
+		if (reporter_value == "csv")
+			GetConfig().SetReporter(make_uniq<reporter::CsvReporter>());
+		else if (reporter_value == "json")
+			GetConfig().SetReporter(make_uniq<reporter::JsonReporter>());
+		else if (reporter_value == "console")
+			GetConfig().SetReporter(make_uniq<reporter::ConsoleReporter>());
+		else
+			std::cerr << "ERROR: unknown reporter: " << reporter_value << '\n';
+	}
 }
 
 } // namespace
